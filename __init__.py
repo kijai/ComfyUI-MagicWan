4e17a3b6063bbce62ee4e5ace40b99ac77c7fbb9
Zack Abrams <36209780+zackabrams@users.noreply.github.com>
2025-02-28 10:58:29 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,25 @@
+from .nodes.wan_flowedit_nodes import WanFlowEditGuiderNode, WanFlowEditGuiderAdvNode, WanFlowEditGuiderCFGNode, WanFlowEditGuiderCFGAdvNode, WanFlowEditSamplerNode
+from .nodes.modify_wan_model_node import ConfigureModifiedWanNode
+from .nodes.wan_model_pred_nodes import WanInverseModelSamplingPredNode, WanReverseModelSamplingPredNode
+
+NODE_CLASS_MAPPINGS = {
+    "WanFlowEditGuider": WanFlowEditGuiderNode,
+    "WanFlowEditGuiderAdv": WanFlowEditGuiderAdvNode,
+    "WanFlowEditGuiderCFG": WanFlowEditGuiderCFGNode,
+    "WanFlowEditGuiderCFGAdv": WanFlowEditGuiderCFGAdvNode,
+    "WanFlowEditSampler": WanFlowEditSamplerNode,
+    "ConfigureModifiedWan": ConfigureModifiedWanNode,
+    "WanInverseModelSamplingPred": WanInverseModelSamplingPredNode,
+    "WanReverseModelSamplingPred": WanReverseModelSamplingPredNode,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "WanFlowEditGuider": "Wan FlowEdit Guider",
+    "WanFlowEditGuiderAdv": "Wan FlowEdit Guider Advanced",
+    "WanFlowEditGuiderCFG": "Wan FlowEdit Guider CFG",
+    "WanFlowEditGuiderCFGAdv": "Wan FlowEdit Guider CFG Advanced",
+    "WanFlowEditSampler": "Wan FlowEdit Sampler",
+    "ConfigureModifiedWan": "Configure Modified Wan Model",
+    "WanInverseModelSamplingPred": "Wan Inverse Model Pred",
+    "WanReverseModelSamplingPred": "Wan Reverse Model Pred",
+}
